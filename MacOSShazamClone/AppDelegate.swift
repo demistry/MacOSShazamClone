a68db1ec3e5077383bdcb5e6ed49d92eeb223f6d
David Ilenwabor <dave11ilen@gmail.com>
2021-07-26 05:03:07 +0100

    Initial Commit

---
+++ MacOSShazamClone/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MacOSShazamClone
+//
+//  Created by David Ilenwabor on 26/07/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
